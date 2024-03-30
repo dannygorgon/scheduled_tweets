1ed71175eaa4cc24c5d3d5bd553854ecae223701
dannygorgon <thirdoptionisthename@googlemail.com>
2024-03-30 01:38:50 +0000

    add routes, controller and layout for root

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "about", to: "about#index"
+  root to: "main#index"
 
   # Defines the root path route ("/")
   # root "posts#index"
