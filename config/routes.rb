e7604c920d075a9f6bc88ccedd239e5c76aac25c
dannygorgon <thirdoptionisthename@googlemail.com>
2024-03-30 14:18:18 +0000

    add bootstrap to application layout header. add formatting to homepage h1

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "about", to: "about#index"
-  root to: "main#index"
-
+  
   # Defines the root path route ("/")
   # root "posts#index"
+  root to: "main#index"
 end
