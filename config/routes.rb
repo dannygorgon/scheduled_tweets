06fcbe8ea755dc7b969ab47fc2395addf547efae
dannygorgon <thirdoptionisthename@googlemail.com>
2024-03-31 21:44:36 +0100

    add form and route for sign up

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "about", to: "about#index"
-  
   get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
   # Defines the root path route ("/")
   # root "posts#index"
   root to: "main#index"
