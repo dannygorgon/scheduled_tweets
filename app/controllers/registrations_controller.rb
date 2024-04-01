f8d2a94745f52337ae52d6621e1c28de758a6af9
dannygorgon <thirdoptionisthename@googlemail.com>
2024-04-01 11:32:21 +0100

    fix issue with params  not printing due to turbo

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,4 +2,10 @@ class RegistrationsController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    puts "This is working"
+    params
+    render plain: params[:user]
+  end
 end
