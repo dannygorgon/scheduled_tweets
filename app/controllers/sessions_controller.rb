eada4e32e38011d43dc6e4c03ed325093cb78ca3
dannygorgon <thirdoptionisthename@googlemail.com>
2024-04-01 16:29:13 +0100

    fix type in main controller. add create method with conditional redirects and rendering

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Logged in successfully!"
+    else
+      flash[:alert] = "Invalid email or password"
+      render :new
   end
+end
 
   def destroy
     session[:user_id] = nil
