f43aa3537c571442886dbf5c928e6f2059ab58e3
dannygorgon <thirdoptionisthename@googlemail.com>
2024-04-01 16:07:46 +0100

    add get and post routes for login

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   get "about", to: "about#index"
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+
+  get "sign_up", to: "sessions#new"
+  post "sign_up", to: "sessions#create"
+
   delete "logout", to: "sessions#destroy"
   # Defines the root path route ("/")
   # root "posts#index"
