a39b84aa10bd7af37d59d1f0bb16f7cdaee1040b
dannygorgon <thirdoptionisthename@googlemail.com>
2024-04-04 01:36:49 +0100

    fix order of navbar. fix issue with application controller missing a Current.user variable

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,9 @@
 class SessionsController < ApplicationController
-  def new
-  end
-
   def create
     user = User.find_by(email: params[:email])
     if user.present? && user.authenticate(params[:password])
       session[:user_id] = user.id
+      puts "Current user: #{Current.user.inspect}"
       redirect_to root_path, notice: "Logged in successfully!"
     else
       flash[:alert] = "Invalid email or password"
@@ -14,7 +12,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    puts "Current user before logout: #{Current.user.inspect}"
     session[:user_id] = nil
+    puts "Current user after logout: #{Current.user.inspect}"
     redirect_to root_path, notice: "Logged out"
   end
 end
